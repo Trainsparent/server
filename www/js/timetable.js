65b3e281e013c928abbd975bb9f0350adfedda66
Ben Oxley <ben.oxley@gmail.com>
2016-11-05 16:39:15 +0000

    Added graphing

--- www/js/timetable.js
+++ www/js/timetable.js
@@ -10,15 +10,42 @@ function populateTable() {
             var div = document.createElement("div");
             div.className = "bs-callout bs-callout-success";
             div.id = "callout-navbar-mobile-caveats";
-            
+
             var header = document.createElement("h4");
-            header.innerText = resp[i].origins[0].stationName + " to "+resp[i].destinations[0].stationName;
+            header.innerText = resp[i].origins[0].stationName + " to " + resp[i].destinations[0].stationName;
             var para = document.createElement("p");
             para.innerText = resp[i].scheduledArrivalTime;
+            var graphContainer = document.createElement("div");
+
+            var graph = document.createElement("canvas");
+            graphContainer.style.width = "200px";
+            graphContainer.style.height = "100px";
             div.appendChild(header);
             div.appendChild(para);
-            timetable.appendChild(div);
+            graphContainer.appendChild(graph);
+            para.appendChild(graphContainer);
 
+            timetable.appendChild(div);
+            var myChart = new Chart(graph, {
+                type: 'line',
+                data: {
+                    labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],
+                    datasets: [{
+                        label: 'Minutes Delayed',
+                        data: [0,5,9,12,14,15,14,13,12,11.5,9,7,5,2.5,0],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
         }
     };
     r.send();
