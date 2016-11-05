c8362644a65ee9a1aa29397affc2372034d0a0b5
Ben Oxley <ben.oxley@gmail.com>
2016-11-05 23:24:04 +0000

    Update geolocation behaviour

--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -1,18 +1,49 @@
+function getInitialLocation(){
+            $.get("https://huxley.apphb.com/crs", function (data, status) {
+                var dropdown = document.getElementById("nearestStation");
+                while (dropdown.hasChildNodes()) {
+                    dropdown.removeChild(dropdown.lastChild);
+                }
+                for (var i = 0; i < data.length; i++) {
+                    var opt = document.createElement("option");
+                    opt.value = data[i].crsCode;
+                    opt.textContent = data[i].stationName + " (" + data[i].crsCode + ")";
+                    dropdown.appendChild(opt);
+                }
+            });
+}
+
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, function () {
+            $.get("https://huxley.apphb.com/crs", function (data, status) {
+                var dropdown = document.getElementById("nearestStation");
+                while (dropdown.hasChildNodes()) {
+                    dropdown.removeChild(dropdown.lastChild);
+                }
+                for (var i = 0; i < data.length; i++) {
+                    var opt = document.createElement("option");
+                    opt.value = data[i].crsCode;
+                    opt.textContent = data[i].stationName + " (" + data[i].crsCode + ")";
+                    dropdown.appendChild(opt);
+                }
+            });
+        });
     }
 }
 function showPosition(position) {
-    $.get("https://data.gov.uk/data/api/service/transport/naptan_railway_stations/nearest?lat="+position.coords.latitude+"&lon="+position.coords.longitude
-    ,function(data, status){
-        var dropdown = document.getElementById("nearestStation");
-        for (i = 0; i < data.result.length; i++) { 
-            opt = document.createElement("option");
-            opt.value = data.result[i].crscode;
-            opt.textContent = data.result[i].stationname;
-            dropdown.appendChild(opt);
-        }
-    });
-    
+    $.get("https://data.gov.uk/data/api/service/transport/naptan_railway_stations/nearest?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude
+        , function (data, status) {
+            var dropdown = document.getElementById("nearestStation");
+            while (dropdown.hasChildNodes()) {
+                    dropdown.removeChild(dropdown.lastChild);
+                }
+            for (var i = 0; i < data.result.length; i++) {
+                var opt = document.createElement("option");
+                opt.value = data.result[i].crscode;
+                opt.textContent = data.result[i].stationname + " (" + data.result[i].crscode + ")";
+                dropdown.appendChild(opt);
+            }
+        });
+
 }
