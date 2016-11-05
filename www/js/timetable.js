a1d378bd10880d67a376f006649f33ba47f84733
Ben Oxley <ben.oxley@gmail.com>
2016-11-05 18:01:44 +0000

    Moved to huxley API

--- www/js/timetable.js
+++ www/js/timetable.js
@@ -1,9 +1,14 @@
-function populateTable() {
+function populateTable(){
+    populateTableImpl("https://huxley.apphb.com/all/big/from/vic/10?accessToken=2eddeea6-ec45-408b-99cb-8ef5b1d3d6cb");
+    populateTableImpl("https://huxley.apphb.com/all/vic/from/big/10?accessToken=2eddeea6-ec45-408b-99cb-8ef5b1d3d6cb");
+}
+
+function populateTableImpl(url) {
     var r = new XMLHttpRequest();
-    r.open("GET", "http://darwin.hacktrain.com/api/train/BTN/from/VIC?apiKey=2eddeea6-ec45-408b-99cb-8ef5b1d3d6cb&rows=10", true);
+    r.open("GET", url, true);
     r.onreadystatechange = function () {
         if (r.readyState != 4 || r.status != 200) return;
-        var resp = JSON.parse(r.response);
+        var resp = JSON.parse(r.response).trainServices;
         var timetable = document.getElementById("timetable");
 
         for (var i = 0; i < resp.length; i++) {
@@ -12,10 +17,10 @@ function populateTable() {
             div.id = "callout-navbar-mobile-caveats";
 
             var header = document.createElement("h4");
-            header.innerText = resp[i].origins[0].stationName + " to " + resp[i].destinations[0].stationName;
+            header.innerText = resp[i].origin[0].locationName + " to " + resp[i].destination[0].locationName;
             var para = document.createElement("p");
-            para.innerText = "Scheduled arrival time: "+resp[i].scheduledArrivalTime + " Estimated arrival time: "+resp[i].estimatedArrivalTime;
-            if (!(resp[i].scheduledArrivalTime===resp[i].estimatedArrivalTime)&&(!(resp[i].estimatedArrivalTime==="On time"))){
+            para.innerText = "Scheduled arrival time: "+resp[i].sta + " Estimated arrival time: "+resp[i].eta;
+            if (!(resp[i].sta===resp[i].eta)&&(!(resp[i].eta==="On time"))){
                 div.className = "bs-callout bs-callout-warning";
             }
             var graphContainer = document.createElement("div");
