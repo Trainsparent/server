af658a1cf72c6ddb80261b0f18568d63c2050bd3
Ben Oxley <ben.oxley@gmail.com>
2016-11-06 12:20:33 +0000

    Added demo tweaks for presentation

--- www/js/timetable.js
+++ www/js/timetable.js
@@ -38,48 +38,81 @@ function populateTableImpl(station1) {
             
             var para = document.createElement("p");
             para.innerText = "Scheduled departure time: "+resp[i].std + " Estimated departure time: "+resp[i].etd;
+            var warningContainer = document.createElement("i");
+            var textDiv = document.createElement("i");
             if (!(resp[i].std===resp[i].etd)&&(!(resp[i].etd==="On time"))){
                 if (resp[i].etd==="Cancelled"||resp[i].etd==="Delayed"){
                     div.className = "bs-callout bs-callout-danger";
+                    warningContainer.className = "fa-stack fa-lg fa-4x";
+                    warningContainer.style.color = "#6d1500";
+                    var circle = document.createElement("i");
+                    textDiv.innerHTML = "<b>"+Math.round(Math.random()*100)+" min delay likely</b>"
+                    circle.className = "fa fa-circle fa-stack-2x";
+                    trainwarn = document.createElement("i");
+                    trainwarn.className = "fa fa-bolt fa-stack-1x fa-inverse";
+                    warningContainer.appendChild(circle);
+                    warningContainer.appendChild(trainwarn);
                 } else {
                     div.className = "bs-callout bs-callout-warning";
+                    warningContainer.className = "fa-stack fa-lg fa-4x";
+                    warningContainer.style.color = "#ffc677";
+                    circle = document.createElement("i");
+                    circle.className = "fa fa-circle fa-stack-2x";
+                    var trainwarn = document.createElement("i");
+                    
+                    textDiv.innerHTML = "<b>"+Math.round(Math.random()*10)+" min delay likely</b>"
+                    trainwarn.className = "fa fa-train fa-stack-1x fa-inverse";
+                    warningContainer.appendChild(circle);
+                    warningContainer.appendChild(trainwarn);
+
                 }
                 
+            } else {
+                warningContainer.className = "fa-stack fa-lg fa-4x";
+                warningContainer.style.color = "#55ff55";
+                var circle = document.createElement("i");
+                circle.className = "fa fa-circle fa-stack-2x";
+                var trainwarn = document.createElement("i");
+                textDiv.innerText = "No Delays";
+                trainwarn.className = "fa fa-train fa-stack-1x fa-inverse";
+                warningContainer.appendChild(circle);
+                warningContainer.appendChild(trainwarn);
             }
             var graphContainer = document.createElement("div");
             graphContainer.id = "graphContainer";
-
-            var graph = document.createElement("canvas");
+                                
+            //var graph = document.createElement("canvas");
             graphContainer.style.width = "200px";
             graphContainer.style.height = "100px";
             col1.appendChild(header);
             col1.appendChild(para);
-            graphContainer.appendChild(graph);
+            graphContainer.appendChild(warningContainer);
+            col2.appendChild(textDiv);
             col2.appendChild(graphContainer);
             row.appendChild(col1);
             row.appendChild(col2);
             div.appendChild(row);
             timetable.appendChild(div);
-            var myChart = new Chart(graph, {
-                type: 'line',
-                data: {
-                    labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],
-                    datasets: [{
-                        label: 'Minutes Delayed',
-                        data: [0,5,9,12,14,15,14,13,12,11.5,9,7,5,2.5,0],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
+            // var myChart = new Chart(graph, {
+            //     type: 'line',
+            //     data: {
+            //         labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],
+            //         datasets: [{
+            //             label: 'Minutes Delayed',
+            //             data: [0,5,9,12,14,15,14,13,12,11.5,9,7,5,2.5,0],
+            //             borderWidth: 1
+            //         }]
+            //     },
+            //     options: {
+            //         scales: {
+            //             yAxes: [{
+            //                 ticks: {
+            //                     beginAtZero: true
+            //                 }
+            //             }]
+            //         }
+            //     }
+            // });
         }
         
     };
